6be073a597a8a0ea7493aa21602bf89d613da2da
Luke Woodward <luke.woodward@10up.com>
2013-02-23 10:01:26 -0800

    remove unused code as it seems to be causing an error.

--- github.php
+++ github.php
@@ -6,10 +6,3 @@ class GitHub_Deploy extends Deploy {
 		
 	}
 }
-new GitHub_Deploy;
-
-// This is just an example
-	/*$postdata = json_decode( stripslashes( $_POST['payload'] ), true );
-	if ( 'master' === $postdata['commits'][0]['branch'] )
-		$deploy = new Deploy( '/home2/woodwas4/public_html/ashwoodward/wp-content/' );
-		*/
