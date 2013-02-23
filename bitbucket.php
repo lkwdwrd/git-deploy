6da6875ed290a7c9a0fc3f62fcfcfcd97251c006
Luke Woodward <luke.woodward@10up.com>
2013-02-23 10:17:50 -0800

    add proper qualifier

--- bitbucket.php
+++ bitbucket.php
@@ -13,10 +13,10 @@ class BitBucket_Deploy extends Deploy {
 	function __construct( $payload ){
 		$payload = json_decode( stripslashes( $_POST['payload'] ), true );
 		$name = $payload['repository']['name'];
-		error_log( var_export( isset( parent::$repos[ $name ] ), true ) );
-		if ( isset( parent::$repos[ $name ] ) && $repos[ $name ]['branch'] === $payload['commits'][0]['branch'] ){
+		if ( isset( parent::$repos[ $name ] ) && parent::$repos[ $name ]['branch'] === $payload['commits'][0]['branch'] ){
 			$data = parent::$repos[ $name ];
 			$data['commit'] = $payload['commits'][0]['node'];
+			error_log( var_export( $data, true ) );
 			parent::__construct( $data );
 		}
 	}
