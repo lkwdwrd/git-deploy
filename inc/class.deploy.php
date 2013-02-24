87c9b9eab4d27d652d220d69fdc0854efe35bd23
Luke Woodward <luke.woodward@10up.com>
2013-02-24 11:27:06 -0800

    Figuring out where I broke it :)

--- inc/class.deploy.php
+++ inc/class.deploy.php
@@ -1,8 +1,9 @@
 <?php
+error_log( var_export( ! defined( 'ACTIVE_DEPLOY_ENDPOINT' ), true ) );
 // Causes the script to die if we are not using an actual enpoint to access it.
 if( ! defined( 'ACTIVE_DEPLOY_ENDPOINT' ) || ! ACTIVE_DEPLOY_ENDPOINT )
 	die( '<h1>No Access</h1><p>An enpoint needs to be defined to use this file.</p>' );
-error_log( 'getting here!' );
+
 /**
  * The main Deploy class. This is set up for GIT repos.
  *
