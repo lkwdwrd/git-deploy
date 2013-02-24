9983c2636bdecdb37c03b42f681ebbb09c9783e8
Luke Woodward <luke.woodward@10up.com>
2013-02-23 18:24:53 -0800

    logging to see the branch name

--- bitbucket.php
+++ bitbucket.php
@@ -13,6 +13,7 @@ class BitBucket_Deploy extends Deploy {
 	function __construct( $payload ){
 		$payload = json_decode( stripslashes( $_POST['payload'] ), true );
 		$name = $payload['repository']['name'];
+		$this->log( $payload['commits'][0]['branch'] );
 		if ( isset( parent::$repos[ $name ] ) && parent::$repos[ $name ]['branch'] === $payload['commits'][0]['branch'] ){
 			$data = parent::$repos[ $name ];
 			$data['commit'] = $payload['commits'][0]['node'];
