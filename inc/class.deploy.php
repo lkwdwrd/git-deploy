51a7c0faa3893234564b11d02bde0a453190d8df
Arnold Roa <manolet@gmail.com>
2015-10-18 15:02:11 -0500

    thrown an error if there is some error on git pull

--- inc/class.deploy.php
+++ inc/class.deploy.php
@@ -212,6 +212,9 @@ abstract class Deploy {
 			// Update the local repository
 			exec( 'git pull ' . $this->_remote . ' ' . $this->_branch . ' 2>&1', $output, $return_var );
 
+			if ($return_var != 0)
+				throw new Exception("Git pull failed", 1);
+
 			// Secure the .git directory
 			echo exec( 'chmod -R og-rx .git' );
 
@@ -224,8 +227,10 @@ abstract class Deploy {
 
 			if (DEPLOY_FULL_OUTPUT)
 				$this->log( $output );
+
 		} catch ( Exception $e ) {
 			$this->log( $e, 'ERROR' );
+			$this->log( $output );
 		}
 	}
 }
